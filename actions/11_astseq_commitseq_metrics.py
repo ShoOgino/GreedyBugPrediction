1e15741bf941d889a213f8ac1b1289d8781979ea
ShoOgino <loginologist@gmail.com>
2021-07-23 03:14:49 +0900

    fix

--- actions/11_astseq_commitseq_metrics.py
+++ actions/11_astseq_commitseq_metrics.py
@@ -11,7 +11,7 @@ import datetime
 
 # cfg(タスクの設定)を更新
 cfg.clear()
-cfg.pathConfigFile              = __file__
+cfg.pathConfigFile              = os.path.abspath(__file__)
 cfg.project                     = "egit"
 cfg.release                     = 2
 cfg.purpose                     = [cfg.Purpose.searchHyperParameter, cfg.Purpose.searchParameter, cfg.Purpose.test]
@@ -22,8 +22,8 @@ cfg.adoptingCrossValidation     = False
 cfg.splitSize4Validation        = 5
 cfg.epochs4EarlyStopping        = 10
 cfg.period4HyperParameterSearch = 60*60*24
-cfg.id                          = os.path.splitext(os.path.basename(__file__))[0] + "_" + cfg.project + "_" + str(cfg.release)
-cfg.pathDirOutput               = os.path.dirname(os.path.dirname(__file__)) + "/results/" + cfg.id + "_"+str(datetime.datetime.today().strftime("%Y%m%d_%H%M%S"))
+cfg.id                          = os.path.splitext(os.path.basename(cfg.pathConfigFile))[0] + "_" + cfg.project + "_" + str(cfg.release)
+cfg.pathDirOutput               = os.path.dirname(os.path.dirname(cfg.pathConfigFile)) + "/results/" + cfg.id + "_"+str(datetime.datetime.today().strftime("%Y%m%d_%H%M%S"))
 
 # 実験を実行
 maneger = Maneger()
--- actions/9_codemetrics_processmetrics.py
+++ actions/9_codemetrics_processmetrics.py
@@ -11,19 +11,20 @@ import datetime
 
 # cfg(タスクの設定)を更新
 cfg.clear()
-cfg.pathConfigFile              = __file__
-cfg.project                     = "egit"
+cfg.pathConfigFile              = os.path.abspath(__file__)
+cfg.project                     = "cassandra"
 cfg.release                     = 2
-cfg.purpose                     = [cfg.Purpose.searchHyperParameter, cfg.Purpose.searchParameter, cfg.Purpose.test]
-cfg.typesInput                  = [cfg.TypeInput.astseq, cfg.TypeInput.commitseq]
+#cfg.purpose                     = [cfg.Purpose.searchHyperParameter, cfg.Purpose.searchParameter, cfg.Purpose.test]
+cfg.purpose                     = [cfg.Purpose.searchParameter, cfg.Purpose.test]
+cfg.typesInput                  = [cfg.TypeInput.codemetrics, cfg.TypeInput.processmetrics]
 cfg.pathsSampleTrain            = ["C:/Users/login/data/workspace/MLTool/datasets/{}/output/R{}_r_train".format(cfg.project, cfg.release)]
 cfg.pathsSampleTest             = ["C:/Users/login/data/workspace/MLTool/datasets/{}/output/R{}_r_test".format(cfg.project, cfg.release)]
 cfg.adoptingCrossValidation     = False
 cfg.splitSize4Validation        = 5
 cfg.epochs4EarlyStopping        = 10
 cfg.period4HyperParameterSearch = 60*60*24
-cfg.id                          = os.path.splitext(os.path.basename(__file__))[0] + "_" + cfg.project + "_" + str(cfg.release)
-cfg.pathDirOutput               = os.path.dirname(os.path.dirname(__file__)) + "/results/" + cfg.id + "_"+str(datetime.datetime.today().strftime("%Y%m%d_%H%M%S"))
+cfg.id                          = os.path.splitext(os.path.basename(cfg.pathConfigFile))[0] + "_" + cfg.project + "_" + str(cfg.release)
+cfg.pathDirOutput               = os.path.dirname(os.path.dirname(cfg.pathConfigFile)) + "/results/" + cfg.id + "_"+str(datetime.datetime.today().strftime("%Y%m%d_%H%M%S"))
 
 # 実験を実行
 maneger = Maneger()
